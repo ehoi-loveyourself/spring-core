08be8cc450cca30791e69b8d0004eba9f4ac0164
belle <ehoi.loveyourself@gmail.com>
2024-12-19 23:21:44 +0900

    feat: 빈 스코프 - 1. 싱글톤 스코프

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,38 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean bean1 = ac.getBean(SingletonBean.class);
+        SingletonBean bean2 = ac.getBean(SingletonBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+        assertThat(bean1).isSameAs(bean2);
+
+        ac.close(); // 스프링 컨텍스트가 종료되면 destroy() 가 호출되어야 한다.
+    }
+
+    @Scope("singleton") // default 값이지만 공부를 위해서 작성
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
