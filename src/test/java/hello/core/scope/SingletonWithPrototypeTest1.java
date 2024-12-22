e3947a4b0517cab6bcd1ee36fb72f6b563cf531d
belle <ehoi.loveyourself@gmail.com>
2024-12-22 11:58:50 +0900

    feat: 빈 스코프 - 2. 프로토타입 스코프
    프로토타입 빈 직접 요청

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,10 +2,13 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -14,15 +17,30 @@ public class SingletonWithPrototypeTest1 {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
         PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
         bean1.addCount();
-        Assertions.assertThat(bean1.getCount()).isEqualTo(1);
+        assertThat(bean1.getCount()).isEqualTo(1);
 
         PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
         bean2.addCount();
-        Assertions.assertThat(bean2.getCount()).isEqualTo(1);
+        assertThat(bean2.getCount()).isEqualTo(1);
 
         /* 프로토타입 빈을 직접 요청하면 새로운 인스턴스 객체를 만들어서 리턴하게 되고
          * 위의 코드에서 각각의 count가 1이 된다
          * */
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+
+        /* prototypeBean이 clientBean에 주입되어 처음부터 생성되어 있었으므로 각각의 count가 1이어야 하는데 2로 올라간다*/
     }
 
     @Scope("prototype")
@@ -46,6 +64,17 @@ public class SingletonWithPrototypeTest1 {
         @PreDestroy
         public void destroy() {
             System.out.println("PrototypeBean.destroy");
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
         }
     }
 }
